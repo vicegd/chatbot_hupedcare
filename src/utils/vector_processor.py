f08b20ea63a3ca6b0ee4768531db13de1620a667
Vicente <vicegd@gmail.com>
2026-04-08 14:41:06 +0200

    Taking parameters from config.yaml

--- src/utils/vector_processor.py
+++ src/utils/vector_processor.py
@@ -6,27 +6,26 @@ import utils.helper as helper
 
 def update_vector_db():
     config = helper.config
-    # 1. Configuración de API (OpenAI Oficial)
+    # 1. API Configuration (OpenAI Oficial)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
         api_key=os.getenv("MODEL_API_KEY"),
-        model_name=config['ai']['embedding_model']
+        model_name=config['embeddings']['embedding_model']
     )
 
-    # 2. Leer Master Context
+    # 2. Read Master Context
     master_path = os.path.join(config['storage']['data_folder'], "master_context.txt")
     with open(master_path, "r", encoding="utf-8") as f:
         full_text = f.read()
 
-    # 3. Chunking AGRESIVO
-    # Subimos el tamaño a 1500 para que las descripciones de imágenes NO se corten
+    # 3. Chunking
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=config['chunks']['max_chunk_size'],
-        chunk_overlap=config['chunks']['chunk_overlap'],
+        chunk_size=config['embeddings']['max_chunk_size'],
+        chunk_overlap=config['embeddings']['chunk_overlap'],
         separators=["\n--- SOURCE: ", "\n[DB | ", "\n\n"]
     )
     chunks = text_splitter.split_text(full_text)
 
-    # 4. ChromaDB limpio
+    # 4. ChromaDB Connection
     db_path = os.path.join(config['storage']['data_folder'], "vector_db")
     client = chromadb.PersistentClient(path=db_path)
     
@@ -35,9 +34,9 @@ def update_vector_db():
     
     collection = client.create_collection(name="rag_context", embedding_function=openai_ef)
 
-    # 5. Insertar
+    # 5. Insert
     collection.add(
         documents=chunks,
         ids=[f"id_{i}" for i in range(len(chunks))]
     )
-    print(f"Éxito: {len(chunks)} fragmentos indexados con OpenAI.")
+    print(f"Success: {len(chunks)} fragments indexed with OpenAI.")
